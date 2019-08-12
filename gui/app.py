d97e79c36cb17510b96a1d18eaa5b9384ab1ea54
shawnhu <40317966+ShawnHuXiaohan@users.noreply.github.com>
2019-08-12 10:21:21 +0800

    GUI

--- gui/app.py
+++ gui/app.py
@@ -1,49 +1,46 @@
-import cv2
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5.QtWidgets import (QMainWindow, QPushButton, QHBoxLayout, QVBoxLayout, QDesktopWidget, QWidget,
-                             QLabel, QLineEdit, QAction, QFileDialog)
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (QPushButton, QHBoxLayout, QVBoxLayout, QDesktopWidget, QWidget,
+                             QLabel, QLineEdit, QFileDialog)
 
 
 class MainWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.init_ui()
 
-    def init_ui(self):
         # -----Widgets-----
-        load_button = QPushButton("Load")
-        pred_button = QPushButton("Identify")
-        copy_button = QPushButton("Copy")
-        display_bar = QLineEdit(self)
-        display_img = QLabel()
+        self.load_button = QPushButton("Load")
+        self.pred_button = QPushButton("Identify")
+        self.copy_button = QPushButton("Copy")
+        self.display_bar = QLineEdit(self)
+        self.display_img = QLabel()
 
         # -----Adjust-----
+        self.display_img.setFixedSize(970, 550)
+        self.display_img.setStyleSheet("QLabel{background:white}")
 
         # -----Layout-----
+        # Row 1
         main_layout = QVBoxLayout()
-        main_layout.addWidget(display_img)
-
-        # Operation bar: horizontal layout
+        main_layout.addWidget(self.display_img, 1, Qt.AlignCenter)
+        # Row 2
         display_line = QHBoxLayout()
-        display_line.addWidget(load_button, 1)
-        display_line.addWidget(display_bar, 5)
-        display_line.addWidget(pred_button, 1)
-        display_line.addWidget(copy_button, 1)
+        display_line.addWidget(self.load_button, 1)
+        display_line.addWidget(self.display_bar, 5)
+        display_line.addWidget(self.pred_button, 1)
+        display_line.addWidget(self.copy_button, 1)
         main_layout.addLayout(display_line)
 
         # -----Action-----
         # Open file action
-        load_button.clicked.connect(self.show_openfile_dialog)
-
-        # display_bar.textChanged[str].connect(self) # self.method
+        self.load_button.clicked.connect(self.show_openfile_dialog)
 
         # -----Windows-----
         self.setLayout(main_layout)
-        self.resize(1000, 618)
-        self.center()
+        self.setFixedSize(1000, 618)
         self.setWindowTitle("Bankcard Recognizer")
+        self.center()
         self.show()
 
     def center(self):
@@ -55,12 +52,17 @@ class MainWindow(QWidget):
 
     def show_openfile_dialog(self):
         # Action for showing openfile dialog
-        fname = QFileDialog.getOpenFileName(self, caption="Open file", directory="c:", filter="*.png *.jpg *.jpeg")
+        name, type = QFileDialog.getOpenFileName(self,
+                                                 caption="Open file",
+                                                 directory="c:",
+                                                 filter="Image(*.png *.jpg *.jpeg)")
+        img = QPixmap(name).scaled(self.display_img.width(), self.display_img.height())
+        self.display_img.setPixmap(img)
 
-        if fname[0]:
-            with open(fname[0], 'r') as f:
-                img = cv2.imread(f)
-                pix = QPixmap(img)
+    def predict(self):
+        # Action for identify the bankcard
+        pass
 
-                # self.widget.setXXX(img)
-
+    def copy_to_clipboard(self):
+        # Action for copying the identify result to clipboard
+        pass
