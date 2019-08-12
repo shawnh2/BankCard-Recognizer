1b6147e7062a090a3780812f9c63169fdf1c86c8
shawnhu <40317966+ShawnHuXiaohan@users.noreply.github.com>
2019-08-12 11:47:49 +0800

    make GUI pretty

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
+# BankCard-Recognizer with GUI
+#
+# Author: Shawn Hu    © Copyright 2019
+# License: MIT
+#
+# Usage: Run this demo with Python.
+#
+# Requirements: Numpy, Scipy, OpenCV2, Cython, Keras, TensorFlow-GPU.
+
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui.app import MainWindow
--- gui/app.py
+++ gui/app.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtWidgets import (QPushButton, QHBoxLayout, QVBoxLayout, QDesktopWidget, QWidget,
                              QLabel, QLineEdit, QFileDialog)
 
@@ -8,6 +8,7 @@ class MainWindow(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.prefix = "gui/icon/"
 
         # -----Widgets-----
         self.load_button = QPushButton("Load")
@@ -19,6 +20,16 @@ class MainWindow(QWidget):
         # -----Adjust-----
         self.display_img.setFixedSize(970, 550)
         self.display_img.setStyleSheet("QLabel{background:white}")
+        self.load_button.setFixedHeight(30)
+        self.load_button.setIcon(QIcon(self.prefix + "load.png"))
+        self.load_button.setToolTip("Load in an image about bankcard.")
+        self.pred_button.setFixedHeight(30)
+        self.pred_button.setIcon(QIcon(self.prefix + "predict.png"))
+        self.pred_button.setToolTip("Start to identify the bankcard number.")
+        self.copy_button.setFixedHeight(30)
+        self.copy_button.setIcon(QIcon(self.prefix + "copy.png"))
+        self.copy_button.setToolTip("Copy to your clipboard.")
+        self.display_bar.setFixedHeight(30)
 
         # -----Layout-----
         # Row 1
@@ -33,13 +44,15 @@ class MainWindow(QWidget):
         main_layout.addLayout(display_line)
 
         # -----Action-----
-        # Open file action
         self.load_button.clicked.connect(self.show_openfile_dialog)
+        self.pred_button.clicked.connect(self.predict)
+        self.copy_button.clicked.connect(self.copy_to_clipboard)
 
         # -----Windows-----
         self.setLayout(main_layout)
         self.setFixedSize(1000, 618)
         self.setWindowTitle("Bankcard Recognizer")
+        self.setWindowIcon(QIcon(self.prefix + "bankcard.png"))
         self.center()
         self.show()
 
@@ -56,13 +69,27 @@ class MainWindow(QWidget):
                                                  caption="Open file",
                                                  directory="c:",
                                                  filter="Image(*.png *.jpg *.jpeg)")
-        img = QPixmap(name).scaled(self.display_img.width(), self.display_img.height())
-        self.display_img.setPixmap(img)
-
-    def predict(self):
-        # Action for identify the bankcard
-        pass
+        if name:
+            img = QPixmap(name).scaled(self.display_img.width(), self.display_img.height())
+            self.display_img.setPixmap(img)
 
     def copy_to_clipboard(self):
         # Action for copying the identify result to clipboard
+        text = self.display_bar.text()
+        try:
+            import win32clipboard as win
+            import win32con
+
+            win.OpenClipboard()
+            win.EmptyClipboard()
+            win.SetClipboardData(win32con.CF_TEXT, text)
+            win.CloseClipboard()
+            self.copy_button.setText("OK")
+            self.copy_button.setToolTip("Now is in your clipboard you can also continue.")
+        except ImportError:
+            self.copy_button.setText("Fail")
+            self.copy_button.setToolTip("Failure reason: missing win32con in python.")
+
+    def predict(self):
+        # Action for identify the bankcard
         pass
